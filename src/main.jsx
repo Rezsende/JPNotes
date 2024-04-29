9b90e6d89bc15756196f924eb5dc624ac553d82b
Rezsende <jpresendejava@gmail.com>
2024-04-28 22:55:02 -0300

    implemento o Globalstyles

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import GlobalStyled from './style/global'
 import { ThemeProvider } from 'styled-components'
 import theme from './style/theme.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme ={theme}>
+      <GlobalStyled/>
       <App />
     </ThemeProvider>
   </React.StrictMode>,
