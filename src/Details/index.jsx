2df29033fad3c486cb4c90261bda8b418fc18baa
João Paulo <jpresendejava@gmail.com>
2024-05-07 11:35:38 -0400

    criação do header

--- src/Details/index.jsx
+++ src/Details/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Container} from './styles'
+import {Header} from '../components/Header/index'
 import {Button} from '../components/Button/index'
 export function Details() {
     return (
         <Container>
-            
+            <Header/>
             <Button title = "voltar"  />
         </Container>
     );
